f7c63c845f91cb3300f7540215f810ce92bca2b7
robin <robin.bruneau@gmail.com>
2023-11-01 10:18:27 +0100

    maj

--- Blender_Scene_Manager.py
+++ Blender_Scene_Manager.py
@@ -7,7 +7,7 @@ def generate_blender_scene():
     object_path = "D:/PhD/Dropbox/Data/Data/models/Graphosoma/Graphosoma.obj"
     object_texture_path = "D:/PhD/Dropbox/Data/Data/models/Graphosoma/Graphosoma.png"
     medium_path = ""
-    output_path = ("D:/PhD/Projects/Playing_with_NeuS/data/Boudha_marble_1920_1080_MVPS/")
+    output_path = ("D:/PhD/Projects/Playing_with_NeuS/data/Boudha_marble_612_512_MVPS/")
 
     # GENERATE CAMERAS
     #    - sphere_cameras (cameras on a sphere of choosen radius
@@ -16,19 +16,25 @@ def generate_blender_scene():
     #    - multi_cameras (gives your locations and details)
     cm = CameraManager()
     #size = (320,180)
-    size = (1920, 1080)
+    size = (612, 512)
     # ortho = 1.7
     # perspec = 50
     # cm.sphere_cameras(radius=5,number_cameras=20,lens=4,type="orthographic")
-    R = np.load("D:/PhD/Projects/Playing_with_NeuS/data/Graphosoma_MVPS/R.npy")
-    T = np.load("D:/PhD/Projects/Playing_with_NeuS/data/Graphosoma_MVPS/T.npy")
+
+    cam_data = np.load("D:/PhD/Projects/Playing_with_NeuS/data/buddhaPNG_Med100_Im3-60/cameras_unpacked.npz")
+
+    # R = np.load("D:/PhD/Projects/Playing_with_NeuS/data/Graphosoma_RMVS_50/R.npy")
+    # T = np.load("D:/PhD/Projects/Playing_with_NeuS/data/Graphosoma_RMVS_50/T.npy")
+    R = cam_data["R"]
+    T = cam_data["T"]
     C = []
-    for k in range(R.shape[2]):
-        C.append((-R[:,:,k].reshape(3,3).T @ T[:,[k]]).reshape(3))
-    #cm.from_camera_RT(C,lens=50)
+    for k in range(R.shape[0]):
+        C.append((-R[k,:,:].reshape(3,3).T @ T[[k],:]).reshape(3))
+        #C.append((-R[:, :, k].reshape(3, 3).T @ T[:, [k]]).reshape(3))
+    cm.from_camera_RT(C,lens=50)
     cm.size = size
     cm.depth_bit = '16'
-    cm.sphere_cameras(radius=11.5, number_cameras=20, lens=50, type="perspective",size=size)
+    #cm.sphere_cameras(radius=11.5, number_cameras=20, lens=50, type="perspective",size=size)
     #cm.single_camera(location=[-10,0,0],rotation=[0,0,0],lens=1.7,is_looking_at=True,looking_at=[0.0,0.0,0.0],type="orthographic")
     #cm.multi_cameras(locations=[[3,0,0],[0,3,0],[0,0,3]],rotations=[[0,0,0],[0,0,0],[0,0,0]],lens=50,is_looking_at=True,looking_at=[[0.0,0.0,0.0],[0.0,0.0,0.0],[0.0,0.0,0.0]],type="perspective")
     #cm.cameras = [cm.cameras[0]]
